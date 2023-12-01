fb46e591a00ab86f30c528907353a284091da998
achahine78 <achahine78@gmail.com>
2023-12-01 15:37:54 -0500

    day 1 second gold star

--- day1.js
+++ day1.js
@@ -35,6 +35,79 @@ const numberLookup = {
   9: true,
 };
 
+const wordToNumberMap = {
+  zero: "0",
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+};
+
+const numberWords = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+
+const convertStringToTwoDigitNumberWithWords = (str) => {
+  const stringLength = str.length;
+  let leftNum = "";
+  let rightNum = "";
+  for (let i = 0; i < stringLength; i++) {
+    if (numberLookup[str[i]]) {
+      leftNum = String(str[i]);
+      break;
+    } else {
+      const stringSlice = str.slice(0, i + 1);
+      let found;
+      numberWords.map((word) => {
+        if (!found && stringSlice.includes(word)) {
+          found = word;
+        }
+      });
+
+      if (found) {
+        leftNum = wordToNumberMap[found];
+        break;
+      }
+    }
+  }
+
+  for (let i = stringLength - 1; i >= 0; i--) {
+    if (numberLookup[str[i]]) {
+      rightNum = String(str[i]);
+      break;
+    } else {
+      const stringSlice = str.slice(i, stringLength);
+      let found;
+      numberWords.map((word) => {
+        if (!found && stringSlice.includes(word)) {
+          found = word;
+        }
+      });
+
+      if (found) {
+        rightNum = wordToNumberMap[found];
+        break;
+      }
+    }
+  }
+
+  return Number(leftNum + rightNum);
+};
+
 const convertStringToTwoDigitNumber = (str) => {
   const stringLength = str.length;
   let number = "";
@@ -59,11 +132,18 @@ readLinesFromFile(filePath, (err, linesArray) => {
   if (err) {
     console.error("Error reading file:", err);
   } else {
-    const sum = linesArray
+    const oneStarSum = linesArray
       .map(convertStringToTwoDigitNumber)
       .reduce((accumulator, currentValue) => {
         return accumulator + currentValue;
       }, 0);
-    console.log(sum);
+    console.log(oneStarSum);
+
+    const twoStarSum = linesArray
+      .map(convertStringToTwoDigitNumberWithWords)
+      .reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      }, 0);
+    console.log(twoStarSum);
   }
 });
