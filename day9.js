a8632d971b384698a016c30a221c976351a96a5f
achahine78 <achahine78@gmail.com>
2023-12-09 09:31:06 -0500

    day 9 second star

--- day9.js
+++ day9.js
@@ -1,8 +1,6 @@
 const readLinesFromFile = require("./fileReader");
 const filePath = "./day9input.txt";
 
-// const testInput = ["0 3 6 9 12 15", "1 3 6 10 15 21", "10 13 16 21 30 45"];
-
 const checkAllZeros = (array) => {
   for (let i = 0; i < array.length; i++) {
     if (array[i] !== 0) {
@@ -12,7 +10,7 @@ const checkAllZeros = (array) => {
   return true;
 };
 
-const generateNextValue = (sequence) => {
+const generateNextValueOneStar = (sequence) => {
   const extrapolationArray = [sequence];
   let index = 0;
   while (true) {
@@ -34,8 +32,11 @@ const generateNextValue = (sequence) => {
   extrapolationArray[0].push(0);
   for (let i = 1; i < extrapolationArray.length; i++) {
     const previousArray = extrapolationArray[i - 1];
-    const currentArray = extrapolationArray[i]
-    currentArray.push(previousArray[previousArray.length - 1] + currentArray[currentArray.length - 1])
+    const currentArray = extrapolationArray[i];
+    currentArray.push(
+      previousArray[previousArray.length - 1] +
+        currentArray[currentArray.length - 1]
+    );
   }
 
   return sequence.pop();
@@ -45,11 +46,53 @@ const parseInputOneStar = (input) => {
   const sequences = input.map((line) => line.split(" ").map(Number));
   let sum = 0;
   sequences.forEach((sequence) => {
-    sum += generateNextValue(sequence);
+    sum += generateNextValueOneStar(sequence);
   });
-  console.log("oneStar: ", sum)
+  console.log("oneStar: ", sum);
+};
+
+const generateNextValueTwoStar = (sequence) => {
+  const extrapolationArray = [sequence];
+  let index = 0;
+  while (true) {
+    const extrapolationSubarray = [];
+    for (let i = 1; i < extrapolationArray[index].length; i++) {
+      extrapolationSubarray.push(
+        extrapolationArray[index][i - 1] - extrapolationArray[index][i]
+      );
+    }
+    extrapolationArray.push(extrapolationSubarray);
+    index++;
+    if (checkAllZeros(extrapolationSubarray)) {
+      break;
+    }
+  }
+
+  extrapolationArray.reverse();
+
+  extrapolationArray[0].unshift(0);
+  for (let i = 1; i < extrapolationArray.length; i++) {
+    const previousArray = extrapolationArray[i - 1];
+    const currentArray = extrapolationArray[i];
+    currentArray.unshift(
+      previousArray[0] +
+        currentArray[0]
+    );
+  }
+
+  return sequence.shift();
+};
+
+const parseInputTwoStar = (input) => {
+  const sequences = input.map((line) => line.split(" ").map(Number));
+  let sum = 0;
+  sequences.forEach((sequence) => {
+    sum += generateNextValueTwoStar(sequence);
+  });
+  console.log("twoStar: ", sum);
 };
 
 readLinesFromFile(filePath, (err, linesArray) => {
   parseInputOneStar(linesArray);
+  parseInputTwoStar(linesArray);
 });
